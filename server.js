2333f7e9dd03400f01594da5f1e6f7bb1c54867b
Carlos-Melo <carloszdu@gmail.com>
2021-10-11 09:51:27 -0300

    dependences

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+//Importa as dependências que acabamos de instalar
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/desafio-MV'));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + "/dist/desafio-mv"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/desafio-MV/'}),
-);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/desafio-mv/index.html"));
+});
 
-// Start the app by listening on the default Heroku port
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
